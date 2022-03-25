05b92e78dd7edbaec26bb7ac08466a1308c2ccfd
Dave Lilly <dave@lillys.org>
2022-03-25 16:50:43 -0500

    remove .results from json result

--- src/backends/rest.js
+++ src/backends/rest.js
@@ -1,10 +1,10 @@
 export class RestAdapter {
     async getItems(state, filterIds = []) {
-        return (await (await fetch(`${state.params.apiBaseUrl}/products?productIds=${filterIds.join(',')}`)).json()).results
+        return await (await fetch(`${state.params.apiBaseUrl}/products?productIds=${filterIds.join(',')}`)).json()
     }
 
     async search(state) {
-        return { items: (await (await fetch(`${state.params.apiBaseUrl}/products?keyword=${state.searchText}`)).json()).results }
+        return { items: await (await fetch(`${state.params.apiBaseUrl}/products?keyword=${state.searchText}`)).json() }
     }
 
     exportItem(item) {
