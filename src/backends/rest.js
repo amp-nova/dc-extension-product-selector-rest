3c9cb84c3c08649e152a80e43ce9b92a5f53d2cd
Dave Lilly <dave@lillys.org>
2022-05-12 14:27:23 -0500

    Update rest.js

--- src/backends/rest.js
+++ src/backends/rest.js
@@ -1,10 +1,25 @@
+import axios from 'axios'
 export class RestAdapter {
     async getItems(state, filterIds = []) {
-        return await (await fetch(`${state.params.apiBaseUrl}/products?productIds=${filterIds.join(',')}`)).json()
+        // return await (await fetch(`${state.params.apiBaseUrl}/products?productIds=${filterIds.join(',')}`)).json()
+        return (await axios.post(state.params.apiBaseUrl, {
+            args: {
+                productIds: filterIds.join(',')
+            },
+            operation: "getProducts"
+        })).data
     }
 
     async search(state) {
-        return { items: await (await fetch(`${state.params.apiBaseUrl}/products?keyword=${state.searchText}`)).json() }
+        // return { items: await (await fetch(`${state.params.apiBaseUrl}/products?keyword=${state.searchText}`)).json() }
+        return {
+            items: (await axios.post(state.params.apiBaseUrl, {
+                args: {
+                    keyword: state.searchText
+                },
+                operation: "getProducts"
+            })).data
+        }
     }
 
     exportItem(item) {
